ff3203d7ab45a2747e0930541ec1ae4c364c0f49
Marti Jimenez <77886135+Marti2405@users.noreply.github.com>
2022-05-16 17:24:12 +0200

    v5

---
+++ list_of_the_day.py
@@ -0,0 +1,108 @@
+import json
+import random
+
+
+
+#Taille de la liste des mots---------
+taille_liste_mots = 25000
+#------------------------------------
+
+
+
+
+mot_ok = False
+
+while not mot_ok:
+
+    position_mot_voulu = random.randint(47,1000653) #int(input("Entrez la position du mot voulu : ")) 
+
+    with open(r'./w2v.txt' , encoding='utf-8') as f:
+        q = f.readline().split()
+        nbe_mots = q[0]
+        taille_position = q[1]
+        for p in range(position_mot_voulu):
+            x= f.readline()
+        for i in range(1):
+            x = f.readline()
+            y = x.split(" ")
+            motc = y[0].lower()
+            posc = [float(u) for u in y[1::]]
+
+
+    mot = motc
+    pos = posc
+    print("Mot choisit : " , mot)
+    print("----------------------")
+    x = int(input("Le mot vous va-t-il ? (Oui->1,Non->0) : "))
+    if x==1:
+        mot_ok = True
+
+
+#MOT CHOISIT->MOT , POSITION DU MOT CHOISIT->POS
+
+#--------------------------------------------------------
+
+#Calcul de distance
+def distance(x,y):
+    rac = []
+    for i in range(len(x)):
+        w = (x[i]-y[i])**2
+        rac.append(w)
+
+    return (sum(rac))**(1/2)
+
+
+
+
+#----------------------------------------------------------
+liste_mots = ["aaaa","bbbb","cccc"]
+liste_score = [50,60,70]
+
+def update(mot,score):
+    x=taille_liste_mots
+    if liste_score[-1]<score:
+        liste_mots.append(mot)
+        liste_score.append(score)
+    elif liste_score[0]>score:
+        liste_mots.insert(0,mot)
+        liste_score.insert(0,score)
+    else:
+        for i in range(len(liste_mots)):
+            if liste_score[i]>score:
+                liste_mots.insert(i,mot)
+                liste_score.insert(i,score)
+                break
+    if len(liste_mots)>x:
+        liste_mots.pop(-1)
+        liste_score.pop(-1)
+    
+                
+
+
+
+with open(r'./w2v.txt' , encoding='utf-8') as f:
+    q = f.readline().split()
+    nbe_mots = q[0]
+    taille_position = q[1]
+    for w in range(46):
+        x = f.readline()
+    for i in range(int(1000653-46)):
+        x = f.readline()
+        y = x.split(" ")
+        motc = y[0].lower()
+        if mot!=motc:
+            posc = [float(u) for u in y[1::]]
+            dist = distance(pos,posc)
+            update(motc,dist)
+            #print(liste_mots)
+            #print(liste_score)
+
+
+with open(r'./list_of_the_day.json', "w" ) as f:  
+    liste_mots = {"mot":mot,"liste":liste_mots}
+    json.dump(liste_mots,f)
+
+print("Liste crée. Uploadé dans le fichier list_of_the_day.json")
+input()
+
+
---
+++ script2.js
@@ -0,0 +1,46 @@
+import liste_pere from './list_of_the_day.json' assert {type: 'json'};
+$(function(){
+var liste_tous_mots = liste_pere["liste"];
+var mot_victoire = liste_pere["mot"];
+document.getElementById("rejouer").onclick = rejouerclick;
+
+function liste_victoire(){
+    var text = "";
+
+      for (var i = 0; i<30; i++){
+
+          text +="<tr>";
+          text +="<th class=\"number\" id=\"ordre\">"+(i+1)+"</th>";
+          text +="<th class=\"word\" id=\"mots\">"+liste_tous_mots[i]+"</th>";
+          text +="<th class=\"number\" id=\"score\">"+(9999-i)+"</th>";
+          text +="</tr>";
+
+      }
+
+      document.getElementById("guesses_victoire").innerHTML = text;
+      document.getElementById("mot").innerHTML ="Palabra de hoy : "+mot_victoire;
+
+}
+liste_victoire();
+
+function rejouerclick() {
+  window.location.replace('./index.html');
+}
+
+
+var i = 0;
+setInterval(Anim,20);
+
+function Anim(){
+  i += 0.15;
+  var angle = (i+170)%360;
+  var texte = ""
+  texte += "linear-gradient(";
+  texte += angle
+  texte += "deg, rgba(2,0,36,1) 37%, rgba(130,24,66,1) 68%, rgba(255,137,41,1) 100%)"
+  console.log(texte);
+  document.getElementById("bodd").style.background = texte;
+}
+
+
+})
